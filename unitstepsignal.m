f5ddc6f9518b0ad48485aeecca6f37d217d8dedc
bhanuprathap2000 <60665107+bhanuprathap2000@users.noreply.github.com>
2020-05-31 16:29:32 +0530

    Add files via upload

---
+++ unitstepsignal.m
@@ -0,0 +1,9 @@
+n1=-3;
+n2=3;
+n0=0;%location where unit step starts
+function [x,n]=impseq(n0,n1,n2)%function defination 
+n=n1:n2;%range
+x=[(n-n0)>=0];%using this logical relation all values greater than no will return 1
+endfunction
+[x,n]=impseq(n0,n1,n2);%functioncall
+stem(n,x);%discrete sequence plot
