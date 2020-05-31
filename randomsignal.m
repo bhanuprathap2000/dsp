0bea2f9de6885687e9ff85d08ed74cee55f408e3
bhanuprathap2000 <60665107+bhanuprathap2000@users.noreply.github.com>
2020-05-31 17:11:33 +0530

    Add files via upload

---
+++ randomsignal.m
@@ -0,0 +1,8 @@
+N=20;
+x=rand(1,N);%generates the N length random sequence with values ranging between 0 to 1.
+y=randn(1,N);%generates the N length gaussian random sequence with mean of this data 0 and variance 1
+n=0:N-1;
+figure(1);
+stem(n,x);
+figure(2);
+stem(n,y);
