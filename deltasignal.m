2aa6f1fa58176a56afaff74200330295b52bfa62
bhanuprathap2000 <60665107+bhanuprathap2000@users.noreply.github.com>
2020-05-31 16:28:34 +0530

    Update deltasignal.m

--- deltasignal.m
+++ deltasignal.m
@@ -3,7 +3,7 @@ n2=3;
 n0=0;%impulse location
 function [x,n]=impseq(n0,n1,n2)%function defination 
 n=n1:n2;%range
-x=[(n-n0)==0];
+x=[(n-n0)==0];%this condition is used to check when n becomes equal to no then 1 should be returned else 0
 endfunction
 [x,n]=impseq(n0,n1,n2);%functioncall
 stem(n,x);%discrete sequence plot
