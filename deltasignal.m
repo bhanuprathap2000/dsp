6e28f3b7e92785179f7a41bad727e7ff925f5708
bhanuprathap2000 <60665107+bhanuprathap2000@users.noreply.github.com>
2020-05-31 16:22:12 +0530

    Add files via upload

---
+++ deltasignal.m
@@ -0,0 +1,9 @@
+n1=-3;
+n2=3;
+n0=0;%impulse location
+function [x,n]=impseq(n0,n1,n2)%function defination 
+n=n1:n2;%range
+x=[(n-n0)==0];
+endfunction
+[x,n]=impseq(n0,n1,n2);%functioncall
+stem(n,x);%discrete sequence plot
