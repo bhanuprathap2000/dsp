d6440bdb799ab13d86961417886266a64aa6e623
bhanuprathap2000 <60665107+bhanuprathap2000@users.noreply.github.com>
2020-06-06 12:52:11 +0530

    Add files via upload

---
+++ lowpassfilter.m
@@ -0,0 +1,10 @@
+fc=1000;%cutoff frequency
+fs=3000;%sampling frequency
+n=20;%order of fir filter
+w=(2*pi*(fc/fs))/pi;%normalized digital frequency
+[h]=fir1(n,w);%filter coefficents
+[H,W]=freqz(h,1,512);%calculate the frequency response
+figure(1)
+stem(W,abs(H));
+figure(2)
+zplane(h);%plot the zeros on the zplane
